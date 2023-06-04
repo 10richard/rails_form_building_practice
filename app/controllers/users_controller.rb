9a783baeb221307c7a96cab1b4b7c34883852880
Richard Ten <rten04@icloud.com>
2023-06-04 18:54:36 -0400

    Add to edit method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,17 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-        #@user = User.new(user_params)
+        #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
 
         if @user.save
             redirect_to new_user_path and return
         end
-        render :new
+        render :new, status: :unprocessable_entity
+    end
+
+    def edit
+        @user = User.find(params[:id])
     end
 
     private
